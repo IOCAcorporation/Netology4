c9ee7e939953cf6d806b0ce53c0e84bb7ac5b13b
IOCAcorporation <hri.st@yandex.com>
2025-08-17 14:55:50 +0300

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BmiService result = new BmiService();
-        double indexAnswer = result.calculate(17,75_000); //Рост нужно вводить в децимктрах, а вес в грамах.
-        System.out.println(indexAnswer);
-
-    }
-}
-
